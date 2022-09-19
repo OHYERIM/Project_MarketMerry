833a1526dc64e952d879a65ba789a3d99e54932a
OHYERIM <koki57176550@gmail.com>
2022-09-19 16:28:45 +0900

    Initial commit

---
+++ src/main/java/com/example/project_marketmerry/ProjectMarketMerryApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_marketmerry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMarketMerryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMarketMerryApplication.class, args);
+    }
+
+}
